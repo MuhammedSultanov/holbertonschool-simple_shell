89cf193b578b2df992723e98cafc8ccc782bea9f
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-12 02:00:11 +0000

    fixed betty bug and updated code

--- get_input.c
+++ get_input.c
@@ -1,18 +1,25 @@
 #include "main.h"
-
+/**
+ * get_input - this code get input from user
+ *
+ * Return: void
+ */
 char *get_input(void)
-{  
-  char *storage = NULL;
-  size_t size = 0;
-  int a;
+{
+	char *storage = NULL;
+	size_t size = 0;
+	int a;
 
-  a = getline(&storage, &size, stdin);
 
-  if (a == -1)
-  {
-    free(storage);
-    exit(EXIT_FAILURE);
-  }
-
-  return storage;
+	a = getline(&storage, &size, stdin);
+	if (a == -1)
+	{
+		free(storage);
+		exit(EXIT_FAILURE);
+	}
+	if (storage[size - 1] == '\n')
+	{
+		storage[size - 1] = '\0';
+	}
+	return (storage);
 }
