8c2e4576622b1b0da7957b2ab3ca30e4975fdf91
Akuro1411 <nahidisayev147@gmail.com>
2024-05-12 02:41:03 +0000

    Prints environment

---
+++ print_env.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * handle_env - handle the first argv element was a env
+ * @storage: buffer 
+ *
+ * Return: status
+ */
+void handle_env(char *storage)
+{
+	char **env;
+	int i = 0;
+
+	env = environ;
+
+	while (env[i] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	free(storage);
+	exit(EXIT_SUCCESS);
+}
+
